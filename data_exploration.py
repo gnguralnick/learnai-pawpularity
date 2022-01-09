4ee67f63a4874297a95d6146a4ad4a7da9412e99
Gabe Guralnick <gnguralnick@gmail.com>
2022-01-09 11:58:49 -0600

    load images to data_exploration

--- data_exploration.py
+++ data_exploration.py
@@ -2,6 +2,12 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import glob
+from skimage import io
+import numpy
+
+train_data_path = "data/train.csv"
+train_img_path = "data/train"
 
 df = pd.read_csv('data/train.csv')
 
@@ -22,3 +28,11 @@ def plot_attribute(attribute: str) -> None:
 
 
 plot_attribute("Human")
+
+train_images = glob.glob('data/train/*.jpg')
+image = io.imread(train_images[0])
+
+i, (im1) = plt.subplots(1)
+i.set_figwidth(15)
+im1.imshow(image)
+plt.show()
