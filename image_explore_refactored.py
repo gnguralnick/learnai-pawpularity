4107f74f08c0d236d8a67ff965c8f63216e6e5e0
Gabe Guralnick <gnguralnick@gmail.com>
2022-01-19 18:45:39 -0600

    ensure processed_train gets overwritten when running preprocessing code

--- image_explore_refactored.py
+++ image_explore_refactored.py
@@ -4,8 +4,21 @@ from PIL import Image
 import glob
 import matplotlib.pyplot as plt
 import pandas as pd
+import shutil
+import os
 
 TRAIN_DIR_PATH = 'data/train/'
+PROCESSED_TRAIN_DIR_PATH = 'data/processed_train'
+
+
+def reset_processed_train() -> None:
+    """Removes and recreates the folder for the processed training data to ensure everything is properly overwritten"""
+    if os.path.exists(PROCESSED_TRAIN_DIR_PATH) and os.path.isdir(PROCESSED_TRAIN_DIR_PATH):
+        shutil.rmtree(PROCESSED_TRAIN_DIR_PATH, ignore_errors=True)
+    elif os.path.exists(PROCESSED_TRAIN_DIR_PATH):
+        raise Exception()
+    os.makedirs(PROCESSED_TRAIN_DIR_PATH)
+    os.makedirs(PROCESSED_TRAIN_DIR_PATH + '/all')
 
 
 def show_image_in_order(train_images: list, img_num: int):
@@ -23,7 +36,7 @@ def load_and_drop_train_data(file_path: str):
     and items with no Face"""
     df = pd.read_csv(file_path)
     # dropping all images that are collages or have no faces
-    df.drop(df[(df['Collage'] > 0) & (df['Face'] == 0)].index, inplace=True)
+    df.drop(df[(df['Collage'] != 0) | (df['Face'] == 0)].index, inplace=True)
     return df
 
 
@@ -44,6 +57,7 @@ def process_images_and_scores(df: pd.DataFrame, portraits_only: bool):
                 image = image.convert('L')
                 # TODO: decide on resizing val for landscape?
                 image = image.resize((180, 256)) if portraits_only else image.resize((0, 0))
+
                 image.save(img_path.replace('train', 'processed_train/all'))
                 # save pawpularity score
                 pawpularity_text.write(str(df.at[df.index[df['Id'] == img_id][0], 'Pawpularity']) + '\n')
@@ -55,4 +69,5 @@ if __name__ == '__main__':
     show_image_in_order(train_images, 5)
     df = load_and_drop_train_data('data/train.csv')
     # print("--------df after dropping: \n", df)
+    reset_processed_train()
     process_images_and_scores(df, True)
